64a03554f850af2f075b3b24392785f4a43479d2
Kenichi Kamiya <kachick1@gmail.com>
2015-06-18 04:21:45 +0900

    Fix spec typos

--- spec/module-dbc_spec.rb
+++ spec/module-dbc_spec.rb
@@ -10,11 +10,11 @@ describe Module::DbC do
       expect(@foobar.func(9)).to eq(10)
     end
 
-    it "raises an PreConditionError if the argument block unmatched to condition" do
+    it "raises a PreConditionError if the argument block unmatched to condition" do
       expect{@foobar.func(0)}.to raise_error(Module::DbC::PreConditionError)
     end
 
-    it "raises an PostConditionError if the calculated value unmatched to condition" do
+    it "raises a PostConditionError if the calculated value unmatched to condition" do
       expect{@foobar.func(8)}.to raise_error(Module::DbC::PostConditionError)
     end
 
